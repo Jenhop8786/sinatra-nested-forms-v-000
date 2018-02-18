1cabb731138a1cf0b8f633f2fb4b13ed91b38abb
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-18 01:14:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+
     get '/' do
-      erb :root
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
     end
 
     get '/new' do
@@ -13,14 +15,11 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
-
-      params[:pirate][:ships].each do |a|
-        Ship.new(a)
-      end
-      @ships = Ship.all
-
+      @pirate = Pirate.new(params)
+      @ship_1 = Ship.new(params[:pirate][:ships][0])
+      @ship_2 = Ship.new(params[:pirate][:ships][1])
       erb :"pirates/show"
     end
+
   end
 end
